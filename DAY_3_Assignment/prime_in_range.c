9c1215c933c6817592dd1d46283c99937539f761
akashgupta2442005-commits <akashgupta2442005@gmail.com>
2026-06-03 13:34:11 +0530

    day-3 uploaded

---
+++ DAY_3_Assignment/prime_in_range.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int start, end, i, j, isprime;
+
+    printf("enter the starting number :");
+    scanf("%d", &start);
+
+    printf("enter the ending number :");
+    scanf("%d", &end);
+
+    printf("prime numbers between %d and %d are :\n",start, end);
+
+    for(i = start; i <= end; i++){
+        if(i <= 1)
+        continue;
+
+    isprime = 1;
+
+    for(j = 2; j<=i / 2; j++){
+        if (i % j == 0){
+            isprime = 0;
+            break;
+        }
+    }
+    if (isprime)
+    printf("%d", i);
+    }
+    
+    return 0;
+}
