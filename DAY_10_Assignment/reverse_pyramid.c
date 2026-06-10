71fe7d5c68d6d1a8a3854b9207fe5847642fded2
akashgupta2442005-commits <akashgupta2442005@gmail.com>
2026-06-11 00:00:42 +0530

    day-10 uploaded

---
+++ DAY_10_Assignment/reverse_pyramid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int i, j;
+
+    for(i = 5; i >= 1; i--) {
+
+        // Print leading spaces
+        for(j = 1; j <= 5 - i; j++) {
+            printf(" ");
+        }
+
+        // Print stars
+        for(j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
