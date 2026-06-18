de5eff4150214ad7feb45c0c12ed3e7fadfca2d4
akashgupta2442005-commits <akashgupta2442005@gmail.com>
2026-06-18 08:55:09 +0530

    day-18 uploaded

---
+++ DAY_18_Assignment/binary_search.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, i, key;
+    int low, high, mid, found = 0;
+
+    // Input number of elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    // Input sorted array elements
+    printf("Enter %d elements in sorted order:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input element to search
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    // Binary Search
+    low = 0;
+    high = n - 1;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            found = 1;
+            break;
+        } else if (arr[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    // Display result
+    if (found)
+        printf("Element found at position %d\n", mid + 1);
+    else
+        printf("Element not found.\n");
+    return 0;
+}
