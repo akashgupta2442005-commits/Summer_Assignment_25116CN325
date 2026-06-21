099aef835b1402c52645350690d55499cf325f9e
akashgupta2442005-commits <akashgupta2442005@gmail.com>
2026-06-21 21:05:39 +0530

    day-21 uploaded

---
+++ DAY_21_Assignment/count_vowel_and_consonant.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        char ch = str[i];
+
+        // Convert uppercase to lowercase
+        if (ch >= 'A' && ch <= 'Z')
+            ch = ch + 32;
+
+        // Check if character is an alphabet
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+
+        i++;
+    }
+    printf("Number of vowels = %d\n", vowels);
+    printf("Number of consonants = %d\n", consonants);
+    return 0;
+}
