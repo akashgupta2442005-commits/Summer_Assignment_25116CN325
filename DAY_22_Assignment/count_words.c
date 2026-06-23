fce5ebc1523b79593b7fccda1e2db0caeac602c2
akashgupta2442005-commits <akashgupta2442005@gmail.com>
2026-06-23 10:46:20 +0530

    day-22 uploaded

---
+++ DAY_22_Assignment/count_words.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, count = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ')
+            count++;
+    }
+
+    printf("Number of words = %d\n", count);
+    return 0;
+}
